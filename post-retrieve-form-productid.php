e0a8416f7728ea61e2503922ca3624993c08b62f
somdutt ganguly <tosomdutt@gmail.com>
2020-10-15 18:02:13 +0530

    Update post-retrieve-form-productid.php

--- post-retrieve-form-productid.php
+++ post-retrieve-form-productid.php
@@ -1,4 +1,6 @@
-<html>
+<html><head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
     <body>
         <h2>Enter Product ID for which you want to change category (woocommerce)</h2>
         <form method="get" action="post-retrieve-form-productid-process.php">
@@ -11,5 +13,37 @@
             <input type="submit">
             
         </form>
+        
+        <br/><br/>
+        Or
+        <br/><br/>
+        Comma seperated product id(woocommerce)<br/>
+        <textarea id="postidcommaseperated" rows=5 cols=65></textarea><br/>
+        <br/>
+             Category Id in wordpress
+             
+             <input id="categoryidgenerated" value=""><br/>
+             <br/>
+        <button id="cmdlink" href="#">Generate link</button>
+        <br/><br/>
+        <div id="generatedlink"></div>
+        
+        <script language="javascript">
+            
+            $(document).ready(function(){
+  $("#cmdlink").click(function(){
+      var str = $('#postidcommaseperated').val();
+      var catid = $('#categoryidgenerated').val();
+      var ids = str.split(",");
+        $("#generatedlink").empty();
+
+ids.forEach(function(item) {
+      $("#generatedlink").append("<a target='about:blank' href='post-retrieve-form-productid-process.php?postid=" + item+ "&categoryid=" + catid + "'>Click Product #" + item + "</a><br/><br/>");
+
+});
+  });
+});
+
+        </script>
     </body>
 </html>
